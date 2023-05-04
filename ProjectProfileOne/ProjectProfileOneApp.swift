1da01915c88d9d8a9b7beed5500aa18529810ada
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-05-04 16:56:24 +0300

    Initial Commit

---
+++ ProjectProfileOne/ProjectProfileOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectProfileOneApp.swift
+//  ProjectProfileOne
+//
+//  Created by Dmitro Dohryk on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectProfileOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
